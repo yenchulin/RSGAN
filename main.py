dbb6541e17e873e34263619d7a610326ae9372a7
yenchulin <vicky85412@gmail.com>
2020-03-04 23:13:25 +0800

    Lower batch_size to run sentiment attention

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ tf.app.flags.DEFINE_integer('max_dec_steps', 40, 'max timesteps of decoder (max 
 # Hyperparameters
 tf.app.flags.DEFINE_integer('hidden_dim', 256, 'dimension of RNN hidden states') # for discriminator and generator
 tf.app.flags.DEFINE_integer('emb_dim', 128, 'dimension of word embeddings') # for discriminator and generator
-tf.app.flags.DEFINE_integer('batch_size', 32, 'minibatch size') # for discriminator and generator
+tf.app.flags.DEFINE_integer('batch_size', 24, 'minibatch size') # for discriminator and generator
 tf.app.flags.DEFINE_integer('max_enc_steps', 150, 'max timesteps of encoder (max source text tokens)') # for generator
 #tf.app.flags.DEFINE_integer('max_dec_steps', 200, 'max timesteps of decoder (max summary tokens)') # for generator
 tf.app.flags.DEFINE_integer('min_dec_steps', 35, 'Minimum sequence length of generated summary. Applies only for beam search decoding mode') # for generator
