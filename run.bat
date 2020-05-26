22dc0a0de3a837ee099dc5dc467060d70271f423
yenchulin <vicky85412@gmail.com>
2020-05-26 11:04:56 +0800

    Add hparam in run.bat for easy testing the model

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-python main.py --mode="train_generator"
-python main.py --mode="train_discriminator"
-python main.py --mode="adversarial_train"
+python main.py --g_pre_epoch=1 --d_pre_epoch=1 --adver_epoch=2 --mode="train_generator"
+python main.py --g_pre_epoch=1 --d_pre_epoch=1 --adver_epoch=2 --mode="train_discriminator"
+python main.py --g_pre_epoch=1 --d_pre_epoch=1 --adver_epoch=2 --mode="adversarial_train"
