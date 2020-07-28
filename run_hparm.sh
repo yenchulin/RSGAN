c783ac5bf88177962eb4635f9bc15cd58e83e583
yenchulin <vicky85412@gmail.com>
2020-07-28 14:16:13 +0800

    fix shell script

--- run_hparm.sh
+++ run_hparm.sh
@@ -7,9 +7,9 @@
 # epoch * 5/8
 # both (No AE)
 cd DPGAN_copy
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --auto_encoder=false --mode="train_generator"
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --auto_encoder=false --mode="train_discriminator"
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --auto_encoder=false --mode="adversarial_train"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --auto_encoder=false --mode="train_generator"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --auto_encoder=false --mode="train_discriminator"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --auto_encoder=false --mode="adversarial_train"
 
 cd ..
 mkdir DPGAN_both_multi_doc_LDA_topic5_4
@@ -22,13 +22,13 @@ mv DPGAN_copy/MLE DPGAN_both_multi_doc_LDA_topic5_4
 mv DPGAN_copy/discriminator_test DPGAN_both_multi_doc_LDA_topic5_4
 mv DPGAN_copy/discriminator_train DPGAN_both_multi_doc_LDA_topic5_4
 mv DPGAN_copy/discriminator_result DPGAN_both_multi_doc_LDA_topic5_4
-rmdir -rf DPGAN_copy/__pycache__
+rm -rf DPGAN_copy/__pycache__
 
 # baseline (No AE)
 cd DPGAN_copy
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="train_generator"
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="train_discriminator"
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="adversarial_train"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="train_generator"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="train_discriminator"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="adversarial_train"
 
 cd ..
 mkdir DPGAN_baseline_multi_doc_LDA_topic5_4_1
@@ -41,13 +41,13 @@ mv DPGAN_copy/MLE DPGAN_baseline_multi_doc_LDA_topic5_4_1
 mv DPGAN_copy/discriminator_test DPGAN_baseline_multi_doc_LDA_topic5_4_1
 mv DPGAN_copy/discriminator_train DPGAN_baseline_multi_doc_LDA_topic5_4_1
 mv DPGAN_copy/discriminator_result DPGAN_baseline_multi_doc_LDA_topic5_4_1
-rmdir -rf DPGAN_copy/__pycache__
+rm -rf DPGAN_copy/__pycache__
 
 # aspect (No AE)
 cd DPGAN_copy
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --auto_encoder=false --mode="train_generator"
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --auto_encoder=false --mode="train_discriminator"
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --auto_encoder=false --mode="adversarial_train"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --auto_encoder=false --mode="train_generator"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --auto_encoder=false --mode="train_discriminator"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --auto_encoder=false --mode="adversarial_train"
 
 cd ..
 mkdir DPGAN_aspect_only_multi_doc_LDA_topic5_4
@@ -60,13 +60,13 @@ mv DPGAN_copy/MLE DPGAN_aspect_only_multi_doc_LDA_topic5_4
 mv DPGAN_copy/discriminator_test DPGAN_aspect_only_multi_doc_LDA_topic5_4
 mv DPGAN_copy/discriminator_train DPGAN_aspect_only_multi_doc_LDA_topic5_4
 mv DPGAN_copy/discriminator_result DPGAN_aspect_only_multi_doc_LDA_topic5_4
-rmdir -rf DPGAN_copy/__pycache__
+rm -rf DPGAN_copy/__pycache__
 
 # senti (No AE)
 cd DPGAN_copy
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --aspect_attn=false --auto_encoder=false --mode="train_generator"
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --aspect_attn=false --auto_encoder=false --mode="train_discriminator"
-python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --aspect_attn=false --auto_encoder=false --mode="adversarial_train"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --aspect_attn=false --auto_encoder=false --mode="train_generator"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --aspect_attn=false --auto_encoder=false --mode="train_discriminator"
+python3 main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --aspect_attn=false --auto_encoder=false --mode="adversarial_train"
 
 cd ..
 mkdir DPGAN_senti_only_multi_doc_LDA_topic5_4
@@ -79,4 +79,4 @@ mv DPGAN_copy/MLE DPGAN_senti_only_multi_doc_LDA_topic5_4
 mv DPGAN_copy/discriminator_test DPGAN_senti_only_multi_doc_LDA_topic5_4
 mv DPGAN_copy/discriminator_train DPGAN_senti_only_multi_doc_LDA_topic5_4
 mv DPGAN_copy/discriminator_result DPGAN_senti_only_multi_doc_LDA_topic5_4
-rmdir -rf DPGAN_copy/__pycache__
+rm -rf DPGAN_copy/__pycache__
