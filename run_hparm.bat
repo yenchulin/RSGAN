eaf1d3ae26757f6201973781a725399010f50067
yenchulin <vicky85412@gmail.com>
2020-07-28 14:18:03 +0800

    Change dir name of result for baseline model

--- run_hparm.bat
+++ run_hparm.bat
@@ -29,16 +29,16 @@ python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="adversarial_train"
 
 cd ..
-mkdir DPGAN_baseline_multi_doc_LDA_topic5_4
-move DPGAN_copy\myexperiment DPGAN_baseline_multi_doc_LDA_topic5_4
-move DPGAN_copy\test_max_generated DPGAN_baseline_multi_doc_LDA_topic5_4
-move DPGAN_copy\test_sample_generated DPGAN_baseline_multi_doc_LDA_topic5_4
-move DPGAN_copy\train_sample_generated DPGAN_baseline_multi_doc_LDA_topic5_4
-move DPGAN_copy\pretrain_test_sample_generated DPGAN_baseline_multi_doc_LDA_topic5_4
-move DPGAN_copy\MLE DPGAN_baseline_multi_doc_LDA_topic5_4
-move DPGAN_copy\discriminator_test DPGAN_baseline_multi_doc_LDA_topic5_4
-move DPGAN_copy\discriminator_train DPGAN_baseline_multi_doc_LDA_topic5_4
-move DPGAN_copy\discriminator_result DPGAN_baseline_multi_doc_LDA_topic5_4
+mkdir DPGAN_baseline_multi_doc_LDA_topic5_4_1
+move DPGAN_copy\myexperiment DPGAN_baseline_multi_doc_LDA_topic5_4_1
+move DPGAN_copy\test_max_generated DPGAN_baseline_multi_doc_LDA_topic5_4_1
+move DPGAN_copy\test_sample_generated DPGAN_baseline_multi_doc_LDA_topic5_4_1
+move DPGAN_copy\train_sample_generated DPGAN_baseline_multi_doc_LDA_topic5_4_1
+move DPGAN_copy\pretrain_test_sample_generated DPGAN_baseline_multi_doc_LDA_topic5_4_1
+move DPGAN_copy\MLE DPGAN_baseline_multi_doc_LDA_topic5_4_1
+move DPGAN_copy\discriminator_test DPGAN_baseline_multi_doc_LDA_topic5_4_1
+move DPGAN_copy\discriminator_train DPGAN_baseline_multi_doc_LDA_topic5_4_1
+move DPGAN_copy\discriminator_result DPGAN_baseline_multi_doc_LDA_topic5_4_1
 rmdir /s /q DPGAN_copy\__pycache__
 
 :: aspect (No AE)
