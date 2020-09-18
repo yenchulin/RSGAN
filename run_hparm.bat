448948158ba1c18c774da13bde3c9d41f4d48948
yenchulin <vicky85412@gmail.com>
2020-09-18 16:07:14 +0800

    Change DPGAN to RSGAN

--- run_hparm.bat
+++ run_hparm.bat
@@ -1,80 +1,80 @@
-:: Please first move this file outside DPGAN
-:: and copy DPGAN folder without any result naming DPGAN_copy
+:: Please first move this file outside RSGAN
+:: and copy RSGAN folder without any result naming RSGAN_copy
 
 
 :: epoch * 5/8
 :: both (No AE)
-cd DPGAN_copy
+cd RSGAN_copy
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --auto_encoder=false --mode="train_generator"
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --auto_encoder=false --mode="train_discriminator"
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --auto_encoder=false --mode="adversarial_train"
 
 cd ..
-mkdir DPGAN_both_multi_doc_LDA_topic5_3
-move DPGAN_copy\myexperiment DPGAN_both_multi_doc_LDA_topic5_3
-move DPGAN_copy\test_max_generated DPGAN_both_multi_doc_LDA_topic5_3
-move DPGAN_copy\test_sample_generated DPGAN_both_multi_doc_LDA_topic5_3
-move DPGAN_copy\train_sample_generated DPGAN_both_multi_doc_LDA_topic5_3
-move DPGAN_copy\pretrain_test_sample_generated DPGAN_both_multi_doc_LDA_topic5_3
-move DPGAN_copy\MLE DPGAN_both_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_test DPGAN_both_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_train DPGAN_both_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_result DPGAN_both_multi_doc_LDA_topic5_3
-rmdir /s /q DPGAN_copy\__pycache__
+mkdir RSGAN_both_multi_doc_LDA_topic5_3
+move RSGAN_copy\myexperiment RSGAN_both_multi_doc_LDA_topic5_3
+move RSGAN_copy\test_max_generated RSGAN_both_multi_doc_LDA_topic5_3
+move RSGAN_copy\test_sample_generated RSGAN_both_multi_doc_LDA_topic5_3
+move RSGAN_copy\train_sample_generated RSGAN_both_multi_doc_LDA_topic5_3
+move RSGAN_copy\pretrain_test_sample_generated RSGAN_both_multi_doc_LDA_topic5_3
+move RSGAN_copy\MLE RSGAN_both_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_test RSGAN_both_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_train RSGAN_both_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_result RSGAN_both_multi_doc_LDA_topic5_3
+rmdir /s /q RSGAN_copy\__pycache__
 
 :: baseline (No AE)
-cd DPGAN_copy
+cd RSGAN_copy
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="train_generator"
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="train_discriminator"
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --aspect_attn=false --auto_encoder=false --mode="adversarial_train"
 
 cd ..
-mkdir DPGAN_baseline_multi_doc_LDA_topic5_3
-move DPGAN_copy\myexperiment DPGAN_baseline_multi_doc_LDA_topic5_3
-move DPGAN_copy\test_max_generated DPGAN_baseline_multi_doc_LDA_topic5_3
-move DPGAN_copy\test_sample_generated DPGAN_baseline_multi_doc_LDA_topic5_3
-move DPGAN_copy\train_sample_generated DPGAN_baseline_multi_doc_LDA_topic5_3
-move DPGAN_copy\pretrain_test_sample_generated DPGAN_baseline_multi_doc_LDA_topic5_3
-move DPGAN_copy\MLE DPGAN_baseline_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_test DPGAN_baseline_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_train DPGAN_baseline_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_result DPGAN_baseline_multi_doc_LDA_topic5_3
-rmdir /s /q DPGAN_copy\__pycache__
+mkdir RSGAN_baseline_multi_doc_LDA_topic5_3
+move RSGAN_copy\myexperiment RSGAN_baseline_multi_doc_LDA_topic5_3
+move RSGAN_copy\test_max_generated RSGAN_baseline_multi_doc_LDA_topic5_3
+move RSGAN_copy\test_sample_generated RSGAN_baseline_multi_doc_LDA_topic5_3
+move RSGAN_copy\train_sample_generated RSGAN_baseline_multi_doc_LDA_topic5_3
+move RSGAN_copy\pretrain_test_sample_generated RSGAN_baseline_multi_doc_LDA_topic5_3
+move RSGAN_copy\MLE RSGAN_baseline_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_test RSGAN_baseline_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_train RSGAN_baseline_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_result RSGAN_baseline_multi_doc_LDA_topic5_3
+rmdir /s /q RSGAN_copy\__pycache__
 
 :: aspect (No AE)
-cd DPGAN_copy
+cd RSGAN_copy
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --auto_encoder=false --mode="train_generator"
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --auto_encoder=false --mode="train_discriminator"
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --senti_attn=false --auto_encoder=false --mode="adversarial_train"
 
 cd ..
-mkdir DPGAN_aspect_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\myexperiment DPGAN_aspect_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\test_max_generated DPGAN_aspect_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\test_sample_generated DPGAN_aspect_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\train_sample_generated DPGAN_aspect_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\pretrain_test_sample_generated DPGAN_aspect_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\MLE DPGAN_aspect_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_test DPGAN_aspect_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_train DPGAN_aspect_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_result DPGAN_aspect_only_multi_doc_LDA_topic5_3
-rmdir /s /q DPGAN_copy\__pycache__
+mkdir RSGAN_aspect_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\myexperiment RSGAN_aspect_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\test_max_generated RSGAN_aspect_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\test_sample_generated RSGAN_aspect_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\train_sample_generated RSGAN_aspect_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\pretrain_test_sample_generated RSGAN_aspect_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\MLE RSGAN_aspect_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_test RSGAN_aspect_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_train RSGAN_aspect_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_result RSGAN_aspect_only_multi_doc_LDA_topic5_3
+rmdir /s /q RSGAN_copy\__pycache__
 
 :: senti (No AE)
-cd DPGAN_copy
+cd RSGAN_copy
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --aspect_attn=false --auto_encoder=false --mode="train_generator"
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --aspect_attn=false --auto_encoder=false --mode="train_discriminator"
 python main.py --adver_epoch=25 --g_pre_epoch=100 --d_pre_epoch=150 --aspect_attn=false --auto_encoder=false --mode="adversarial_train"
 
 cd ..
-mkdir DPGAN_senti_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\myexperiment DPGAN_senti_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\test_max_generated DPGAN_senti_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\test_sample_generated DPGAN_senti_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\train_sample_generated DPGAN_senti_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\pretrain_test_sample_generated DPGAN_senti_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\MLE DPGAN_senti_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_test DPGAN_senti_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_train DPGAN_senti_only_multi_doc_LDA_topic5_3
-move DPGAN_copy\discriminator_result DPGAN_senti_only_multi_doc_LDA_topic5_3
-rmdir /s /q DPGAN_copy\__pycache__
+mkdir RSGAN_senti_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\myexperiment RSGAN_senti_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\test_max_generated RSGAN_senti_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\test_sample_generated RSGAN_senti_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\train_sample_generated RSGAN_senti_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\pretrain_test_sample_generated RSGAN_senti_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\MLE RSGAN_senti_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_test RSGAN_senti_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_train RSGAN_senti_only_multi_doc_LDA_topic5_3
+move RSGAN_copy\discriminator_result RSGAN_senti_only_multi_doc_LDA_topic5_3
+rmdir /s /q RSGAN_copy\__pycache__
